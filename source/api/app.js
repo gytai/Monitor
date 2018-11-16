a98116117c5085f66e4661d5b85864df7a4cac67
邰广银 <273650669@qq.com>
2018-11-16 11:53:52 +0800

    add cros support

--- source/api/app.js
+++ source/api/app.js
@@ -32,6 +32,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 添加跨域支持
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods','PUT,POST,GET,DELETE,OPTIONS');
+    res.header('X-Powered-By','3.2.1');
+    next();
+});
+
 app.use('/api', indexRouter);
 app.use('/api/node', nodeRouter);
 
